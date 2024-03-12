fb98299c0e1518c9acdc6e86ee891804b7852598
Jonah Köllner <koellnjo@gmail.com>
2024-03-12 11:48:10 +0100

    basically works. Some quirks with sourcing but ey

--- setup.sh
+++ setup.sh
@@ -1,24 +1,41 @@
 #!/bin/bash
 
 # Check for the Shell configuration file
-if [-f "$HOME/.bashrc"]; then
-   SHELLCONF="$HOME/.bashrc"
-elif [ -f "$HOME/.zshrc"]; then
-  SHELLCONF="$HOME/.zshrc"
+if [[ -f "$HOME/.bashrc" ]]; then
+	SHELLCONF="$HOME/.bashrc"
+elif [[ -f "$HOME/.zshrc" ]]; then
+	SHELLCONF="$HOME/.zshrc"
 else
-  echo "No Shell Config (bashrc or zshrc) file found"
-  exit 1
+ echo "No Shell Config (bashrc or zshrc) file found"
+ exit 1
 fi
 
-mkdir -p "~/.scripts/docker/custom_container/"
-mkdir -p "~/.scripts/docker/init/"
+# Create directories
+mkdir -p "$HOME/.scripts/docker/custom_container/" || { echo "Failed to create directory"; exit 1; }
+mkdir -p "$HOME/.scripts/docker/init/" || { echo "Failed to create directory"; exit 1; }
 
-cp "src/custom_container/Dockerfile" "~/.scripts/docker/custom_container/"
+# Copy Dockerfile
+cp "src/custom_container/Dockerfile" "$HOME/.scripts/docker/custom_container/" || { echo "Failed to copy Dockerfile"; exit 1; }
 
-# Append the debian alias into the shell config file
-echo 'alias debian="docker run -it -v $PWD:code ~/.scripts/docker/custom_container/. /bin/bash"' >> "$SHELLCONF"
+echo "" >> "$SHELLCONF"
+echo "# Custom Container scripts" >> "$SHELLCONF"
 
-chmod +x "src/init/docker_init.sh"
-cp -p "src/init/*" "~/.scripts/docker/init/"
+echo 'alias debian="docker build -t custom_debian:latest $HOME/.scripts/docker/custom_container/ && docker run -it -v $PWD:/code custom_debian:latest /bin/bash "' >> "$SHELLCONF" || { echo "Failed to append the alias to the $SHELLCONF"; exit 1; }
 
-echo 'export "PATH=$PATH:$HOME/.scripts/docker/init/"' >> "$SHELLCONF"
+# Make start docker script executable
+chmod +x "src/init/docker_init.sh" || { echo "Failed to make script executable"; exit 1; }
+
+# Copy initialization scripts
+cp -r "src/init/" "$HOME/.scripts/docker/init/" || { echo "Failed to copy initialization scripts"; exit 1; }
+
+echo 'export "PATH=$PATH:$HOME/.scripts/docker/init/"' >> "$SHELLCONF" || { echo "Failed to add the location $HOME/.scripts/docker/init/ to the PATH in $SHELLCONF"; exit 1; }
+
+# Source new condif
+. "$SHELLCONF" 2>/dev/null
+if [ $? -ne 0 ]; then
+    # Attempt to source the shell configuration file in Zsh
+    zsh -c ". $SHELLCONF 2>/dev/null"
+    if [ $? -ne 0 ]; then
+        echo "[Warning] Couldn't source $SHELLCONF in zsh or bash. Manually sourcing required"
+    fi
+fi
--- src/init/docker_init.sh
+++ src/init/docker_init.sh
@@ -1,15 +1,19 @@
+#!/bin/bash
+
 system=$(uname)
-if [ "$system" -eq "Darwin" ]; then
-	#Mac
-	echo "[script] Mac Sytem Detected."
-	if [ $# -eq 1 ] && [ $1 -eq "42" ]; then
-		echo "[script] Starting 42 mac Init in goinfre"
-		bash mac/42mac_init.sh
-	else
-		echo "[open] Opening docker"
-		open -g -a Docker
-elif [ "$system" -eq "Linux" ]; then
-	#linux
-	echo "[script] Linux System Detected."
-	echo "[systemctl] Starting docker deamon..."
-	sudo systemctl start docker
+if [ "$system" = "Darwin" ]; then
+    # Mac
+    echo "[script] Mac System Detected."
+    if [ $# -eq 1 ] && [ $1 -eq "42" ]; then
+        echo "[script] Starting 42 mac Init in goinfre"
+        bash mac/42mac_init.sh
+    else
+        echo "[open] Opening Docker"
+        open -g -a Docker
+    fi
+elif [ "$system" = "Linux" ]; then
+    # Linux
+    echo "[script] Linux System Detected."
+    echo "[systemctl] Starting Docker daemon..."
+    sudo systemctl start docker
+fi
